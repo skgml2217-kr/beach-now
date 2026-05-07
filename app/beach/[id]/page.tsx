a3a3e4e128db087337b133a16a30d54bebe8bb62
skgml2217-kr <skgml2217@gmail.com>
2026-05-07 17:42:34 +0900

    Update page.tsx

--- app/beach/[id]/page.tsx
+++ app/beach/[id]/page.tsx
@@ -198,7 +198,7 @@ export default async function BeachDetailPage({
           </div>
 
           {/* 길찾기 버튼 */}
-href={`https://map.kakao.com/link/map/${encodeURIComponent(beach.name + ' ' + beach.address)},${beach.location.lat},${beach.location.lng}`}
+            href={`https://map.kakao.com/link/map/${encodeURIComponent(beach.name)},${beach.location.lat},${beach.location.lng}`}
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center gap-2 mt-1 px-4 py-2 rounded-full
